694c078f690825ec3ac8577aa446b298d1177070
moshiur111 <moshiur.code@gmail.com>
2024-12-01 19:18:19 +0600

    Error Handler is added

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import config from "../app/config";
+
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.status || 500
+    
+    res.status(statusCode).json({
+        message: err.message || 'Something went wrong',
+        success: false,
+        error: err.name || "Error",
+        stack: config.node_env === 'development' ? err.stack : undefined,
+    })
+}
+
+export default errorHandler
