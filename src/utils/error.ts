04a563fc1b45f1a32ce5cc7f73d2c034c37fbd51
moshiur111 <moshiur.code@gmail.com>
2024-12-01 19:19:14 +0600

    Custom error function is added

---
+++ src/utils/error.ts
@@ -0,0 +1,8 @@
+class HttpError extends Error {
+  status: number;
+  constructor(message: string = 'Something went wrong', status: number = 500) {
+    super(message);
+    this.status = status;
+  }
+}
+export default HttpError
