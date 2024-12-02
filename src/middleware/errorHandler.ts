0c9f44b0e636295db2c432c1c050e61f5c5e1fc7
moshiur111 <moshiur.code@gmail.com>
2024-12-02 14:16:11 +0600

    err type is added

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,15 +1,22 @@
-import { Request, Response, NextFunction } from "express";
-import config from "../app/config";
+import { Request, Response, NextFunction } from 'express';
+import config from '../app/config';
+import HttpError from '../utils/error';
 
-const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.status || 500
-    
-    res.status(statusCode).json({
-        message: err.message || 'Something went wrong',
-        status: false,
-        error: err.name || "Error",
-        stack: config.node_env === 'development' ? err.stack : undefined,
-    })
-}
+const errorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = err.status || 500;
 
-export default errorHandler
+  res.status(statusCode).json({
+    message: err.message || 'Something went wrong',
+    status: false,
+    error: err.name || 'Error',
+    stack: config.node_env === 'development' ? err.stack : undefined,
+  });
+  next()
+};
+
+export default errorHandler;
