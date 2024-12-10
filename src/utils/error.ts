c5dd504d81b2fbd30b6b4d5b665edf413c6ae55e
moshiur111 <moshiur.code@gmail.com>
2024-12-10 23:00:19 +0600

    modified error utils

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,8 +1,8 @@
-class HttpError extends Error {
+class error extends Error {
   status: number;
   constructor(message: string = 'Something went wrong', status: number = 500) {
     super(message);
     this.status = status;
   }
 }
-export default HttpError
+export default error
